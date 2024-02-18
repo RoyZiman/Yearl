aa9261d765bb69f95df90bc51559821a2740a52f
BuildTools <royziman17@gmail.com>
2024-02-18 19:40:18 +0200

    Initial commit

---
+++ MainProgram/Program.cs
@@ -0,0 +1,54 @@
+﻿using Yearl.Language;
+using Yearl.Language.Syntax;
+
+namespace Yearl
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            const string Path = "<stdIn>";
+
+            bool showTree = true;
+            Dictionary<VariableSymbol, object> variables = new Dictionary<VariableSymbol, object>();
+
+
+            while (true)
+            {
+                Console.Write("> ");
+                string? input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input)) continue;
+
+                if (input == "quit") break;
+
+                if (input == "cls")
+                {
+                    Console.Clear();
+                    continue;
+                }
+
+                if (input == "#tree")
+                {
+                    showTree = !showTree;
+                    Console.WriteLine(showTree ? "Showing parse trees." : "Hiding parse trees.");
+                    continue;
+                }
+
+                SyntaxTree syntaxTree = SyntaxTree.Parse(Path, input);
+                Compilation compilation = new Compilation(Path, input, syntaxTree);
+                EvaluationResult result = compilation.Evaluate(variables);
+
+                if (!result.Errors.Any())
+                {
+                    if (showTree)
+                        Console.WriteLine(syntaxTree);
+                    Console.WriteLine(result.Value);
+                }
+                else foreach (Error Error in result.Errors)
+                        Console.WriteLine(Error);
+
+            }
+        }
+    }
+}
---
+++ Yearl.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Yearl.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
